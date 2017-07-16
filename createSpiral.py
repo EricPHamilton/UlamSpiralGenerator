a1b244bc45f1089c06143ad161683fb4f924ce38
hams <hamilte@purdue.edu>
2017-07-16 11:46:31 -0400

    Edited image creation process.
    Did this to reduce runtime memory usage and clean up code.

--- createSpiral.py
+++ createSpiral.py
@@ -2,31 +2,24 @@ from PIL import Image
 import sys
 width = int(sys.argv[1])
 
-string = "";
-
-# Load in the entire file in chunks of 4 into a string variable.
-with open('output.txt', 'r') as output:
-    string = output.read()
-
-print("Read done, file closed.")
-
 im= Image.new('RGB', (width, width), "white")
-
 pixels = im.load()
 width, height = im.size
 
 rowCtr = 0
 colCtr = 0
 
-for letter in string:
-	if letter == 'X':
-		pixels[colCtr, rowCtr] = (0, 0, 0)
-		colCtr += 1
-	elif letter == ' ':
-		colCtr += 1
-	elif letter == '\n':
-		rowCtr += 1
-		colCtr = 0;
+with open('output.txt', 'r') as output:
+    for line in output:
+        for letter in line:
+        	if letter == 'X':
+        		pixels[colCtr, rowCtr] = (0, 0, 0)
+        		colCtr += 1
+        	elif letter == ' ':
+        		colCtr += 1
+        	elif letter == '\n':
+        		rowCtr += 1
+        		colCtr = 0;
 
 pixels[height/2, width/2] = (255, 0, 0)
 im.save("spiral.png");
